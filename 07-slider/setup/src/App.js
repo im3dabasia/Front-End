608d170e8a11d05049b871cda3dabb0e56de7c02
im3dabasia <eshaandabasiya@gmail.com>
2022-06-15 18:18:25 +0530

    project 7 done midway

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,81 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [review,setReview] = useState([]);
+  const [index,setIndex] = useState(1);
+
+
+  const checkIndex =(num) =>{
+    if (num > (data.length -1)){
+      return 0
+    }
+  }
+
+  const nextSlide = () => {
+    const temp = index
+    setIndex(checkIndex(temp+1))
+  } 
+
+  const lastSlide = () => {
+    const temp = index
+    setIndex(checkIndex(temp-1))
+  } 
+
+
+  
+
+
+
+
+  return (<section className='section'>
+    <h2 className='title'> <span>/</span>Reviews</h2>
+
+    <div className='section-center'>
+
+
+
+      {data.map((item,revIndex) => {
+          const {id,image,name, title,quote} = item;
+
+          let position = 'nextSlide';
+          if (revIndex === index ){
+            position = 'activeSlide'
+          }
+          if (revIndex === (index-1)){
+            position = 'lastSlide'
+          }
+        
+          
+          return(
+            <article className={position} key={id}>
+              <img src={image} className='person-img'></img>
+              <h4> {title}</h4>
+              <p>{title}</p>
+              <p className='text'>{quote}</p>
+
+              <button onClick={lastSlide}  className='icon prev'>
+                <FiChevronLeft/>
+
+              </button>
+              <button  onClick={nextSlide} className='icon next'>
+                <FiChevronRight/>
+              </button>
+              < FaQuoteRight className='icon'/>
+
+
+            </article>
+
+
+          )
+
+
+
+      })}
+
+
+    </div>
+
+  </section>)
 }
 
 export default App;
